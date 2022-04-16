147c3293b7d68aeb1fc7d3fcc05ef624b87e0aae
Your Name <you@example.com>
2022-04-16 12:45:42 +0300

    react commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
+      <District name="Dhaka" special="Tanjim Hasan"></District>
+      <District name="Borisal" special="Tech Tree"></District>
+      <District name="Khulna" special="Amir khan"></District>
     </div>
   );
 }
 
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+function Post(props) {
+  return (
+    <div className='Postss'>
+      <h2>Posts: {props.title}</h2>
+      <p>Body: {props.body}</p>
+    </div>
+  )
+}
+function District(props) {
+  const [power, setPower] = useState([1]);
+
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower)
+  }
+
+  return (
+    <div className='Districts'>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.special}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower}>Boost The Power</button>
+    </div>
+  )
+}
 export default App;
